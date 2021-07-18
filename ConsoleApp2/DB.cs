a6d3b5ff9412d689e44618bd9a9d274f474e1193
pilylkin2 <62954333+pilylkin2@users.noreply.github.com>
2021-07-19 00:08:23 +0300

    Add files via upload

---
+++ ConsoleApp2/DB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost; port = 3306; username=root;  database=bithday");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ ConsoleApp2/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int number { get; set; }
+        public int mouth { get; set; }
+        public string clas { get; set; }
+        
+        public Data (int Id, string Name, int number, int mouth, string clas)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.number = number;
+            this.mouth = mouth;
+            this.clas = clas;
+        }
+    }
+}
